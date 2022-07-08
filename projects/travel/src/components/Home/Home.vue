0aefa13aea2da5a3870c9450abde6fd83035da97
chenyue <chenyue.com>
2022-07-08 17:36:26 +0800

    多页应用VS单页应用

--- projects/travel/src/components/Home/Home.vue
+++ projects/travel/src/components/Home/Home.vue
@@ -1,5 +1,16 @@
+<!--
+    template只能向外暴露一个标签:当超过一个标签的时候要另外加一个标签包裹起来
+-->
 <template>
-    <div>home</div>
+    <div>
+        <div>home</div>
+        <!--
+            跳转页面引入:router-link元素中的to属性
+            /代表根路径
+        -->
+        <router-link to="/Hello world">Hello world</router-link>
+    </div>
+    
 </template>
 
 <script>
